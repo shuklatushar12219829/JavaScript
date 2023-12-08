026e134d1428da58b62e59b9bf7f3335690e3619
Tushar Shukla 12219829 <115138208+shuklatushar12219829@users.noreply.github.com>
2023-12-09 01:21:15 +0530

    Create 03_Datatypes.js

---
+++ 03_Datatypes.js
@@ -0,0 +1,15 @@
+// nn bb ss u PRIMITIVE DATATYPES
+let a=null
+let b=234
+let c="Tushar"
+let d=Symbol("hello everyone")
+let e=undefined
+let f=true
+let g=BigInt("299")+BigInt("1")
+console.log(a,b,c,d,e,f,g)
+//NON-PRIMITIVE DATATYPE-objects in js
+const item={
+  "name" : "Led bulb",
+  "price" : 123
+}
+console.log(item["name"])
