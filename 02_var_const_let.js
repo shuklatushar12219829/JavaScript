875e1105e544a7216cef80c35d43aba6f67e7d1f
Tushar Shukla 12219829 <115138208+shuklatushar12219829@users.noreply.github.com>
2023-12-09 01:20:13 +0530

    Create 02_var_const_let.js

---
+++ 02_var_const_let.js
@@ -0,0 +1,11 @@
+let a=3
+var b=4
+const c=5
+{
+  let a="Kill black"
+  var b="hello nigga"
+}
+//const c=7//gives error as c is constant so it's value can't be changed
+console.log(a)
+console.log(b)
+console.log(c)
