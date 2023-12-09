e8a975e273dfbb934a1fb36f9aab6f547b7aac27
Tushar Shukla 12219829 <115138208+shuklatushar12219829@users.noreply.github.com>
2023-12-10 02:16:27 +0530

    Create 05_operator_and_expression.js

---
+++ 05_operator_and_expression.js
@@ -0,0 +1,10 @@
+let a=45
+let b=4
+console.log("a + b = ",a+b)
+console.log("a + b = ",a-b)
+console.log("a + b = ",a/b)
+console.log("a + b = ",a*b)
+console.log("a + b = ",a**b)
+console.log("a++ = ",a++)
+console.log("a = ",a)
+console.log("++a = ",--a)
